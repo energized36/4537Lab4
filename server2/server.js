3e51db333c792a26b4121deea01084ceddbe4b25
Michael <michael.lau@hotmail.ca>
2026-02-13 11:51:55 -0800

    Modify server2 code to fix CORS

--- server2/server.js
+++ server2/server.js
@@ -5,11 +5,6 @@ import { insertPatients, runSelectQuery } from "./db.js";
 class Server2 {
     constructor() {    
         this.server2 = http.createServer(this.handleRequest.bind(this));
-        this.allowedOrigins = [
-            'http://localhost:8888',
-            'http://127.0.0.1:5500',
-            'http://localhost:5500'
-        ];
     }
 
     start() {
@@ -19,15 +14,7 @@ class Server2 {
 
     // add CORS headers to allow cross-origin requests from Server1
     setCORSHeaders(req, resp) {
-        const origin = req.headers.origin;
-        
-        if (this.allowedOrigins.includes(origin)) {
-            resp.setHeader('Access-Control-Allow-Origin', origin);
-        } else {
-            // default to localhost:8888 if origin not in list
-            resp.setHeader('Access-Control-Allow-Origin', `http://localhost:${process.env.FALLBACK_PORT}`);
-        }
-        
+        resp.setHeader('Access-Control-Allow-Origin', '*');
         resp.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
         resp.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     }
