e2140c1fe70c590fa5562fae36d83fcb0f81616d
Michael <michael.lau@hotmail.ca>
2026-02-13 12:15:21 -0800

    Change env variables

--- server2/db.js
+++ server2/db.js
@@ -13,8 +13,8 @@ const writerPool = mysql.createPool({
 const readerPool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  user: "lab4_reader",
-  password: "strongpassword",
+  user: process.env.DB_USER2,
+  password: process.env.DB_PASS2,
   database: process.env.DB_NAME
 });
 
